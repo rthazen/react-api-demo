28aa3816ffd8f4bec2e7eb70bff3e9ec4cfb9389
Reed Hazen <redhazen@gmail.cocm>
2024-08-29 22:19:51 -0700

    different api calls with mock api

---
+++ src/services/postService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+const getPosts = () => api.get("/posts");
+
+const deletePost = (id) => api.delete(`/posts/${id}`);
+
+const createPost = (post) => api.post("/posts", post);
+
+const updatePost = (id, post) => axios.put(`/posts/${id}`, post);
+
+export { getPosts, deletePost, createPost, updatePost };
